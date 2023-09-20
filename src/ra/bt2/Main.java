652b8364baf9394a1c1d7a46fcd298e42a225069
Nguyễn Xuân Thành <nxt1012@gmail.com>
2023-09-20 09:01:31 +0700

    update drawIsoslecesTriangle function in bt2

--- src/ra/bt2/Main.java
+++ src/ra/bt2/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int menuSelection;
         System.out.println("Nhập số để lựa chọn loại hình muốn in từ menu dưới dây: ");
-        System.out.println("1. Hình chữ nhật\n2. Tam giác vuông, góc vuông ở phía dưới, bên trái\n3. Tam giác vuông, góc vuông ở phía trên, bên trái\n");
+        System.out.println("1. Hình chữ nhật\n2. Tam giác vuông, góc vuông ở phía dưới, bên trái\n3. Tam giác vuông, góc vuông ở phía trên, bên trái\n4. Tam giác cân\n5. Thoát");
         menuSelection = sc.nextInt();
         if (menuSelection == 1) {
             System.out.println("Bạn đã lựa chọn vẽ hình chữ nhật");
@@ -28,6 +28,13 @@ public class Main {
             System.out.print("Vui lòng vào chiều dài cạnh góc vuông: ");
             int width = sc.nextInt();
             drawTopLeftRightTriangle(width);
+        } else if (menuSelection == 4) {
+            System.out.println("Bạn đã lựa chọn vẽ hình tam giác cân");
+            System.out.print("Vui lòng vào chiều cao của tam giác: ");
+            int height = sc.nextInt();
+            drawIsoslecesTriangle(height);
+        } else if (menuSelection == 5) {
+            System.exit(0);
         }
     }
 
@@ -61,4 +68,25 @@ public class Main {
         }
     }
 
+    public static void drawIsoslecesTriangle(int height) {
+//        vẽ hình tam giác có chiều cao là height
+//        chiều dài cạnh đáy là 2*height -1
+//        => trong ma trận vuông height x height, vẽ * tại các vị trí như sau:
+//        dòng 1: index = (1, height)
+//        dòng 2: index = (2, height-1) ; (2, height); (2, height+1)
+//        dòng 3: index = (3, height-2); (3, height-1); (3, height); (3, height+1); (3, height+2)
+//        dòng i: index = (i, height-i+1);...(i, height);...; (i, height+i-1)
+//        i lớn nhất là height
+        for (int i = 1; i <= height; i++) {
+            for ( int j = 1; j <= height; j++){
+                if (j >= height - i + 1 &&j <= height + i - 1){
+                    System.out.print("* ");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
 }
